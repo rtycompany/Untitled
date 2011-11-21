28bc22d7392369769d3988f7a6a0b5800738d033
Matt Tuttle <heardtheword@gmail.com>
2011-11-21 09:14:00 -0600

    Adding comments

--- src/com/christiandevs/entities/Character.java
+++ src/com/christiandevs/entities/Character.java
@@ -57,7 +57,7 @@ public abstract class Character extends Entity
 		super(x, y);
 		
 		state = PlayState.Wait;
-		skills = new HashMap<SkillType, Integer>();
+		initSkills();
 		
 		health = new Stat(50);
 		energy = new Stat(20);
@@ -69,6 +69,22 @@ public abstract class Character extends Entity
 		moveSpaces = 3;
 	}
 	
+	/**
+	 * Creates a skill hashmap with all values initialized to 1
+	 */
+	private void initSkills()
+	{
+		skills = new HashMap<SkillType, Integer>();
+		SkillType[] types = SkillType.values();
+		for (int i = 0; i < types.length; i++)
+		{
+			skills.put(types[i], 1);
+		}
+	}
+	
+	/**
+	 * Called when a character levels up
+	 */
 	public void levelUp()
 	{
 		level += 1;
@@ -76,6 +92,10 @@ public abstract class Character extends Entity
 		health.refill();
 	}
 	
+	/**
+	 * Character takes a hit
+	 * @param value the amount of damage taken
+	 */
 	public void takeDamage(int value)
 	{
 		health.drain(value);
@@ -83,26 +103,46 @@ public abstract class Character extends Entity
 			kill();
 	}
 	
+	/**
+	 * Check if the character is dead
+	 * @return Am I Dead?
+	 */
 	public boolean isDead()
 	{
 		return health.depleted();
 	}
 	
+	/**
+	 * Kills off the character
+	 */
 	public void kill()
 	{
 		scene.remove(this);
 	}
 	
+	/**
+	 * Starts the character turn by setting the state
+	 */
 	public void startTurn()
 	{
 		state = PlayState.StartTurn;
 	}
 	
+	/**
+	 * Are we still waiting for the character to do something?
+	 * @return character is still thinking
+	 */
 	public boolean isWaiting()
 	{
 		return (state == PlayState.Wait);
 	}
 	
+	/**
+	 * A dumb easing function that moves the camera
+	 * @param start the start point
+	 * @param target the end point
+	 * @return the amount to ease
+	 */
 	private float ease(float start, float target)
 	{
 		if (Math.abs(target - start) < 4)
@@ -114,6 +154,10 @@ public abstract class Character extends Entity
 		return start + 2;
 	}
 	
+	/**
+	 * Moves or snaps the camera to focus on the character
+	 * @param smooth moves gradually over time
+	 */
 	public void focusCamera(boolean smooth)
 	{
 		float targetX = x - Engine.width / 2 + sprite.frameWidth / 2;
@@ -131,16 +175,14 @@ public abstract class Character extends Entity
 		}
 	}
 	
+	/**
+	 * How aware is this character of another
+	 * @param c The character to check against
+	 * @return a value between 0 and 1 on how aware we are of the character
+	 */
 	protected float awareness(Character c)
 	{
-		return 0;
-	}
-	
-	protected int getSkillLevel(SkillType skill)
-	{
-		if (skills.containsKey(skill))
-			return skills.get(skill);
-		
+		// back facing should be something like 0.25
 		return 0;
 	}
 	
@@ -148,14 +190,14 @@ public abstract class Character extends Entity
 	{
 		if (weapon == null)
 			return 1;
-		return weapon.power + getSkillLevel(weapon.skill);
+		return weapon.power + skills.get(weapon.skill);
 	}
 	
 	protected int getArmorRating()
 	{
 		if (armor == null)
 			return 0;
-		return armor.defense + getSkillLevel(armor.skill);
+		return armor.defense + skills.get(armor.skill);
 	}
 	
 	protected SkillType getArmorSkill()
@@ -176,12 +218,16 @@ public abstract class Character extends Entity
 	
 	protected void increaseSkill(SkillType skill, int value)
 	{
-		int current = 0;
-		if (skills.containsKey(skill))
-			current = skills.get(skill);
-		skills.put(skill, current + value);
+		skills.put(skill, skills.get(skill) + value);
 	}
 	
+	/**
+	 * Finds an enemy and attacks if one is found
+	 * @param type the enemy type to search for
+	 * @param dx the x-axis position to search at
+	 * @param dy the y-axis position to search at
+	 * @return if we successfully attacked an enemy
+	 */
 	protected boolean attack(String type, int dx, int dy)
 	{
 		Entity e = scene.findAt(type, dx, dy);
@@ -192,6 +238,11 @@ public abstract class Character extends Entity
 		return attack(enemy);
 	}
 	
+	/**
+	 * Attacks an enemy character
+	 * @param enemy the character to attack
+	 * @return if we successfully attacked the character
+	 */
 	protected boolean attack(Character enemy)
 	{
 		fatigue.buff(stamina);
@@ -203,6 +254,7 @@ public abstract class Character extends Entity
 			// Missed enemy, increase skill
 			enemy.increaseSkill(enemy.getArmorSkill(), 1);
 			fatigue.drain(1);
+			return false;
 		}
 		else
 		{
@@ -217,11 +269,21 @@ public abstract class Character extends Entity
 		return true;
 	}
 	
+	/**
+	 * Sets the map value
+	 * @param map the map object this character exists on
+	 */
 	public void setMap(Map map)
 	{
 		this.map = map;
 	}
 	
+	/**
+	 * Check if we can move to a destination
+	 * @param dx the x-axis destination
+	 * @param dy the y-axis destination
+	 * @return if we can move to that point
+	 */
 	protected boolean canMoveTo(int dx, int dy)
 	{
 		LinkedList<PathNode> nodes = new LinkedList<PathNode>();
@@ -229,9 +291,16 @@ public abstract class Character extends Entity
 		return pathContains(nodes, dx / map.tileWidth, dy / map.tileHeight);
 	}
 	
-	private boolean pathContains(List<PathNode> nodes, int x, int y)
+	/**
+	 * Check if a path contains a point
+	 * @param path the path to check
+	 * @param x
+	 * @param y
+	 * @return if the path contains the point
+	 */
+	private boolean pathContains(List<PathNode> path, int x, int y)
 	{
-		Iterator<PathNode> it = nodes.iterator();
+		Iterator<PathNode> it = path.iterator();
 		while (it.hasNext())
 		{
 			PathNode node = it.next();
@@ -241,6 +310,13 @@ public abstract class Character extends Entity
 		return false;
 	}
 	
+	/**
+	 * Determine the walkable area of the character (recursive)
+	 * @param nodes a list of nodes to populate with the walkable area
+	 * @param x the starting x-axis value
+	 * @param y the starting y-axis value
+	 * @param spaces the number of spaces away from the starting point
+	 */
 	protected void getWalkableArea(List<PathNode> nodes, int x, int y, int spaces)
 	{
 		if (spaces <= 0) return;
